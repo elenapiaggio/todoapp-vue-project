2ff7292537f69c1f0baacb3f7d4406ba7035d8ea
Elena Piaggio <elenapiaggio@gmail.com>
2025-05-17 12:45:57 +0200

    ADD meta requires

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { supabase } from '@/clients/supabase';
 
 import Login from '@/views/LoginView.vue';
 
@@ -18,8 +19,22 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/TasksView.vue'),
+      meta: { requiresAuth: true }
     },
   ],
+})
+
+router.beforeEach(async (to) => {
+  if (to.meta.requiresAuth) {
+    // recupera la sesión actual
+    const { data: { session } } = await supabase.auth.getSession()
+
+    // si no hay sesión, redirige a login
+    if (!session) {
+      return { name: 'login' }
+    }
+  }
+  // si no requiere auth o hay sesión, sigue adelante
 })
 
 export default router
