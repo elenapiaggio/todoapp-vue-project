d4e9239b73a0b116e6439f0f6323a2449e0d6aa7
Elena Piaggio <elenapiaggio@gmail.com>
2025-06-02 23:49:27 +0200

    ADD desktop style

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div class="task-form">
+    <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
+      placeholder="Add new task ..." />
+    <button @click="onAddTask" class="btn-addtask">Add Task</button>
+  </div>
+</template>
+
+<style scoped>
+.task-form {
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  margin-top: 1rem;
+}
+
+.task-form input {
+  padding: 0.75rem;
+  font-size: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+}
+
+.btn-addtask {
+  padding: 0.75rem;
+  font-size: 1rem;
+  border: none;
+  border-radius: 6px;
+  background-color: #73B5BD;
+  color: white;
+  cursor: pointer;
+  font-weight: 500;
+}
+
+.btn-addtask:hover {
+  opacity: 0.9;
+}
+</style>
---
+++ src/components/TaskSummary.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { computed } from 'vue';
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  tasks: {
+    type: Array,
+    required: true,
+  },
+});
+
+const todoCount = computed(() => props.tasks.filter(t => !t.is_complete).length);
+const doneCount = computed(() => props.tasks.filter(t => t.is_complete).length);
+
+</script>
+
+<template>
+
+  <div class="summary">
+    <h3>Summary</h3>
+    <ul>
+      <li>Todo: {{ todoCount }}</li>
+      <li>Done: {{ doneCount }}</li>
+    </ul>
+  </div>
+
+</template>
+
+
+<style scoped>
+.summary {
+  padding: 1.5rem;
+  border-radius: 10px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.05);
+  font-family: sans-serif;
+  color: #555561;
+  min-width: 150px;
+  margin-right: 1rem;
+}
+
+.summary h3 {
+  margin-bottom: 1rem;
+  font-size: 1.2rem;
+  color: #555561;
+}
+
+.summary ul {
+  list-style-type: disc;
+  padding-left: 1.2rem;
+}
+
+.summary li {
+  margin-bottom: 0.5rem;
+  font-size: 1rem;
+}
+</style>
